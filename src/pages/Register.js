26f7b38a3ff7e2511a0fd8b20b09bec4982bd10b
Naunau <ahmadhanifnaufaljamil@gmail.com>
2025-11-14 22:36:44 +0700

    fixed header footer

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,23 +2,25 @@ import './register.css';
 
 function Register() {
   return (
-    <div className="wrapper">
-        <form action="">
-            <h1>Register</h1>
-            <div class="input-box">
-                <input type="email" placeholder="Email" required />
-                <i className='bx bx-envelope'></i>
-            </div>
-            <div className="input-box">
-                <input type="text" placeholder="Username" required />
-                <i className='bx bx-user'></i>
-            </div>
-            <div className="input-box">
-                <input type="password" placeholder="Password" required />
-                <i className='bx bx-lock-alt'></i>
-            </div>
-            <button type="submit" className="btn">Register</button>
-        </form>
+    <div class="main-body">
+        <div className="wrapper">
+            <form action="">
+                <h1>Register</h1>
+                <div class="input-box">
+                    <input type="email" placeholder="Email" required />
+                    <i className='bx bx-envelope'></i>
+                </div>
+                <div className="input-box">
+                    <input type="text" placeholder="Username" required />
+                    <i className='bx bx-user'></i>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required />
+                    <i className='bx bx-lock-alt'></i>
+                </div>
+                <button type="submit" className="btn">Register</button>
+            </form>
+        </div>
     </div>
   );
 }
