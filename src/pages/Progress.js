1f59d5f7e763c9ce71212f9df948eb5d33be2eb4
Naunau <ahmadhanifnaufaljamil@gmail.com>
2025-11-21 14:34:20 +0700

    progress finishing

--- src/pages/Progress.js
+++ src/pages/Progress.js
@@ -1,13 +1,13 @@
 import './Progress.css';
-import _1 from '../assets/check-white.png';
 import _2 from '../assets/box-white.png';
 import _3 from '../assets/cargo-truck-white.png';
-import _4 from '../assets/shopping-cart-white.png';
+import _1 from '../assets/shopping-cart-white.png';
+import _4 from '../assets/check-white.png';
 
 function Progress() {
     return (
         <div class="progress-body">
-            <div class="banner">
+            <div class="progress-banner">
                 <h1>Order Tracking Page</h1>
                 <p>Your order has been placed.</p>
                 <p>Please wait and track your order progress from here.</p>
@@ -31,26 +31,36 @@ function Progress() {
                 <h2>Estimated Delivery Date: <span class="est-date green">Date</span></h2>
             </div>
             <div class="progress-wrapper progress-sections">
-                <div class="progress-container">
-                    <div class="progress-line">-</div>
-                    <div class="progress-step">
-                        <img src={_1}></img>
+                <div class="progress-sub-wrapper">
+                    <div class="progress-container">
+                        <div class="progress-line"></div>
+                        <div class="progress-step">
+                            <div class="img-border">
+                                <img src={_1}></img>
+                            </div>
+                        </div>
+                        <div class="progress-step">
+                            <div class="img-border">
+                                <img src={_2}></img>
+                            </div>
+                        </div>
+                        <div class="progress-step">
+                            <div class="img-border">
+                                <img src={_3}></img>
+                            </div>
+                        </div>
+                        <div class="progress-step">
+                            <div class="img-border">
+                                <img src={_4}></img>
+                            </div>
+                        </div>
                     </div>
-                    <div class="progress-step">
-                        <img src={_2}></img>
+                    <div class="details-container">
+                        <h1>Order Placed</h1>
+                        <h1>Packaged</h1>
+                        <h1>On the Way</h1>
+                        <h1>Delivered</h1>
                     </div>
-                    <div class="progress-step">
-                        <img src={_3}></img>
-                    </div>
-                    <div class="progress-step">
-                        <img src={_4}></img>
-                    </div>
-                </div>
-                <div class="progress-details">
-                    <h1>order placed</h1>
-                    <h1>order placed</h1>
-                    <h1>order placed</h1>
-                    <h1>order placed</h1>
                 </div>
             </div>
         </div>
